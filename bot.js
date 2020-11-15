0e5bb34ef2378b5571dece71b253091409e85337
spgrammatakis <spgrammatakis@gmail.com>
2020-11-15 21:11:05 +0200

    initial commit
    init

---
+++ bot.js
@@ -0,0 +1,37 @@
+const { Client} = require('discord.js');
+const client = new Client();
+const token = "";
+var fs = require("fs");
+var https = require('https');
+client.login(token);
+
+client.on('ready', () => {
+    console.log('I am ready!');
+  });
+  
+  client.on('message', message => {
+    // If the message is '!rip'
+    if (message.content === 'a') {
+        message.channel.messages.fetch({ limit: 20 })
+            .then(messages =>{
+              messages.forEach(element =>{
+                var attachment = element.attachments.array();
+                  attachment.forEach(value =>{
+                  let string = value.url.substring(value.url.lastIndexOf("/")+1, value.url.length);
+                  console.log(string);
+                  let file = fs.createWriteStream("images/"+ string);
+                  const request = https.get(value.url, function(response) {
+                  response.pipe(file);
+                  });
+                  fs.appendFile('input.txt', value.url + "\n", function(err) {
+                    if (err) {
+                       return console.error(err);
+                    }
+                });
+                })
+              })
+            }
+              )
+        .catch(console.error);
+    }
+  });
